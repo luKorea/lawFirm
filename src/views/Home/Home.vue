e2b8fa028d218dcdd1a1f461c097d30489df94b3
korealu <643949593@qq.com>
2020-08-11 17:55:37 +0800

    'bug修复'

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,12 +5,20 @@
     <div class="black-content">
       <el-col :lg="12" :md="12" :xl="12" :sm="24">
         <p>重要通知：{{ homeInfo.ChimportantNotice }}</p>
+        <p>重要なお知らせ：{{ homeInfo.JaimportantNotice }}</p>
         <p>地址：{{ homeInfo.Chaddress }}</p>
+        <p>住所：{{ homeInfo.Jaaddress }}</p>
         <p>
           邮编：{{ homeInfo.Chpostcode }}
           电子邮箱：{{ homeInfo.Chemail }}
           电话：{{ homeInfo.Chphone }}
           传真：{{ homeInfo.Chfax }}
+        </p>
+        <p>
+          郵便番号：{{ homeInfo.Jaemail }}
+          Eメール：{{ homeInfo.Japostcode }}
+          電話：{{ homeInfo.Japhone }}
+          ファックス：{{ homeInfo.Jafax }}
         </p>
       </el-col>
     </div>
